123afc8279874cdc85d5f58de0a82800e084dceb
steven03.chen <steven03.chen@vipshop.com>
2019-03-29 22:03:08 +0800

    fix defination file

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Page, Browser } from 'puppeteer'
-import {Domkit} from './typings/Domkit'
-
-declare var page: Page
-declare var browser: Browser
-
-declare var $: Domkit;
-
-export default $
